55da9d4aad14302d5c45a3bc8bf6fb404fa912a1
alirezatd80 <alirezatd80@gmail.com>
2025-08-07 15:00:08 +0330

    count letter

--- function.py
+++ function.py
@@ -1,13 +1,18 @@
 import re
+import string
 
-def find_word(string):
-    words = re.findall(r'\w+' , string)
+def find_word(text:str):
+    words = re.findall(r'\w+' , text)
     return words
 
-def find_sentences(string:str):
-    sentences = re.split(r'(?<=[\.\?!])\s*|\n\s*' , string.strip())
+def find_sentences(text:str):
+    sentences = re.split(r'(?<=[\.\?!])\s*|\n\s*' , text.strip())
     return sentences
 
-def count_word(string):
-    words = find_word(string)
+def count_word(text:str):
+    words = find_word(text)
     return {word: words.count(word) for word in words}
+
+def count_lettler(text:str):
+    letters = list(string.ascii_lowercase)
+    return {letter: text.count(letter) for letter in letters}
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from function import find_word,find_sentences,count_word
+from function import find_word,find_sentences,count_word,count_lettler
 
 
 
-print((count_word('hi \n i am alireza.i am a joke?for ever!\ndo you know ? n for n'.strip())))
+print((count_lettler('hi \n i am alireza.i am  joke?for ever!\ndo you know ? n for n'.strip())))
