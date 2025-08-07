d208069beca9c0fe5c9ae6b7d2362436484aceaa
alirezatd80 <alirezatd80@gmail.com>
2025-08-07 14:53:45 +0330

    countword

--- function.py
+++ function.py
@@ -4,4 +4,10 @@ def find_word(string):
     words = re.findall(r'\w+' , string)
     return words
 
+def find_sentences(string:str):
+    sentences = re.split(r'(?<=[\.\?!])\s*|\n\s*' , string.strip())
+    return sentences
 
+def count_word(string):
+    words = find_word(string)
+    return {word: words.count(word) for word in words}
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from function import find_word
+from function import find_word,find_sentences,count_word
 
 
 
+print((count_word('hi \n i am alireza.i am a joke?for ever!\ndo you know ? n for n'.strip())))
